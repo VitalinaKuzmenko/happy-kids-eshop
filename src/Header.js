99b4dcd69dd6f79cb43d3e110fec613d1da49c2d
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-11 20:46:48 +0100

    Created and styled header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Header.css";
+import Login from "./icons/login.svg";
+import Basket from "./icons/cart.svg";
 
 const Header = () => {
   return (
     <>
+      <h1>happy kids</h1>
       <header class="header">
-        <a href="" class="logo">
-          CSS Nav
-        </a>
+        <input className="search-input" type="text" placeholder="Search..." />
         <input class="menu-btn" type="checkbox" id="menu-btn" />
         <label class="menu-icon" for="menu-btn">
           <span class="navicon"></span>
@@ -26,6 +27,16 @@ const Header = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
+        <div className="icons flex content-between mx-8">
+          <div className="login flex items-center mx-5 cursor-pointer">
+            <p className="pr-2">Login</p>
+            <img src={Login} alt="login-icon" />
+          </div>
+          <div className="basket flex items-center px-5 cursor-pointer">
+            <img className="pb-1 " src={Basket} alt="basket-icon" />
+            <p class="text-on-cart">0</p>
+          </div>
+        </div>
       </header>
     </>
   );
