4f55443f634304c8b53cbe0292c434c2c917ec4e
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-12 19:53:54 +0100

    Styled all the footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,57 @@
 import React from "react";
+import "./Footer.css";
+import FacebookIcon from "./icons/icons8-facebook-100.svg";
+import PinterestIcon from "./icons/icons8-pinterest-100.svg";
+import InstagramIcon from "./icons/icons8-instagram-100.svg";
 
 const Footer = () => {
   return (
-    <div>
-      <h4>happy kids</h4>
-      <div>
-        <div className="footer-menu"></div>
-        <div className="icons"></div>
-        <div className="extra-menu"></div>
+    <footer>
+      <hr></hr>
+      <h4 className="mt-8 text-center">happy kids</h4>
+      <div className="flex flex-col justify-around my-8 md:flex-row">
+        <div className="footer-menu flex flex-col my-3 mx-3">
+          <a href="#">Home</a>
+          <a href="#">Shop Collection</a>
+          <a href="#">Our Story</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="footer-icons flex justify-center items-start my-3">
+          <img src={FacebookIcon} alt="facebook-icon" />
+          <img src={PinterestIcon} alt="facebook-icon" />
+          <img src={InstagramIcon} alt="facebook-icon" />
+        </div>
+        <div className="extra-menu flex flex-col my-3 mx-3">
+          <a href="#">Shipping & Returns</a>
+          <a href="#">Store Policy</a>
+          <a href="#">Payment Methods</a>
+          <a href="#">FAQ</a>
+        </div>
       </div>
 
-      <form>
-        <label htmlFor="email">Join Our Mailing List</label>
-        <input type="email" id="email" name="email" required />
-
-        <button type="submit">Subscribe Now</button>
+      <form className="flex flex-col mt-5 mb-10">
+        <label className="text-2xl text-black" htmlFor="email">
+          Join Our Mailing List
+        </label>
+        <div className="mt-5">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email here*"
+            required
+          />
+          <button className="subscribe-button" type="submit">
+            Subscribe Now
+          </button>
+        </div>
+        <p className="mt-3 text-black ">Thank you for subscribing!</p>
       </form>
-      <p>
+      <p className="pb-5">
         This website design was created by Wix.com, and is used here for
         strictly educational purposes.
       </p>
-    </div>
+    </footer>
   );
 };
 
--- src/Product.js
+++ src/Product.js
@@ -5,7 +5,7 @@ import "./Product.css";
 const Product = ({ item }) => {
   return (
     <div className="product flex flex-col">
-      <img src={item.image} alt="item" className="w-65 block my-0 mx-auto" />
+      <img src={item.image} alt="item" className="w-61 block my-0 mx-auto" />
       <h1>{item.name}</h1>
       <p>{item.price}</p>
       <div className="block my-0 mx-auto">
